0b1151a0965b0900c53b1567103b8b71a3033a51
Zahi Farah <zahi.farah@protonmail.com>
2021-09-10 19:16:38 +0300

    Move images into images file. Add some comments.

--- src/Hangman.js
+++ src/Hangman.js
@@ -1,23 +1,29 @@
 import React, { Component } from "react";
 import "./Hangman.css";
-import img0 from "./0.jpg";
-import img1 from "./1.jpg";
-import img2 from "./2.jpg";
-import img3 from "./3.jpg";
-import img4 from "./4.jpg";
-import img5 from "./5.jpg";
-import img6 from "./6.jpg";
+import img0 from "./images/0.jpg";
+import img1 from "./images/1.jpg";
+import img2 from "./images/2.jpg";
+import img3 from "./images/3.jpg";
+import img4 from "./images/4.jpg";
+import img5 from "./images/5.jpg";
+import img6 from "./images/6.jpg";
 
 class Hangman extends Component {
   /** by default, allow 6 guesses and use provided gallows images. */
   static defaultProps = {
     maxWrong: 6,
-    images: [img0, img1, img2, img3, img4, img5, img6]
+    images: [img0, img1, img2, img3, img4, img5, img6],
   };
 
   constructor(props) {
     super(props);
-    this.state = { nWrong: 0, guessed: new Set(), answer: "apple" };
+    this.state = {
+      nWrong: 0,
+      guessed: new Set(),
+      answer: "apple",
+    };
+
+    /** handlers */
     this.handleGuess = this.handleGuess.bind(this);
   }
 
@@ -27,7 +33,7 @@ class Hangman extends Component {
   guessedWord() {
     return this.state.answer
       .split("")
-      .map(ltr => (this.state.guessed.has(ltr) ? ltr : "_"));
+      .map(letter => (this.state.guessed.has(letter) ? letter : "_"));
   }
 
   /** handleGuest: handle a guessed letter:
@@ -35,22 +41,22 @@ class Hangman extends Component {
     - if not in answer, increase number-wrong guesses
   */
   handleGuess(evt) {
-    let ltr = evt.target.value;
+    let letter = evt.target.value;
     this.setState(st => ({
-      guessed: st.guessed.add(ltr),
-      nWrong: st.nWrong + (st.answer.includes(ltr) ? 0 : 1)
+      guessed: st.guessed.add(letter),
+      // if state.answer.includes(letter) evaluates to true and add 0 to nWrong, else add 1
+      nWrong: st.nWrong + (st.answer.includes(letter) ? 0 : 1),
     }));
   }
 
   /** generateButtons: return array of letter buttons to render */
   generateButtons() {
-    return "abcdefghijklmnopqrstuvwxyz".split("").map(ltr => (
+    return "abcdefghijklmnopqrstuvwxyz".split("").map(letter => (
       <button
-        value={ltr}
+        value={letter}
         onClick={this.handleGuess}
-        disabled={this.state.guessed.has(ltr)}
-      >
-        {ltr}
+        disabled={this.state.guessed.has(letter)}>
+        {letter}
       </button>
     ));
   }
@@ -58,11 +64,11 @@ class Hangman extends Component {
   /** render: render game */
   render() {
     return (
-      <div className='Hangman'>
+      <div className="Hangman">
         <h1>Hangman</h1>
         <img src={this.props.images[this.state.nWrong]} />
-        <p className='Hangman-word'>{this.guessedWord()}</p>
-        <p className='Hangman-btns'>{this.generateButtons()}</p>
+        <p className="Hangman-word">{this.guessedWord()}</p>
+        <p className="Hangman-btns">{this.generateButtons()}</p>
       </div>
     );
   }
